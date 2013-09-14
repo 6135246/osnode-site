724c664954cb482bb7c1ea355cc643c21ac0013c
老牛啊 <obullxl@gmail.com>
2013-09-14 20:58:56 +0800

    modify sql Signed-off-by: 老牛啊 <obullxl@gmail.com>

--- lib/db.sql
+++ lib/db.sql
@@ -34,21 +34,6 @@ CREATE TABLE atom_topic (
   gmt_modify datetime
 ) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
 
-ALTER TABLE atom_topic CHANGE visit_count visit bigint DEFAULT '0';
-ALTER TABLE atom_topic CHANGE reply_count reply bigint DEFAULT '0';
-
-ALTER TABLE atom_topic ADD state varchar(20) DEFAULT 'T' AFTER id;
-ALTER TABLE atom_topic ADD topt varchar(20) DEFAULT 'F' AFTER catg;
-ALTER TABLE atom_topic ADD rflag varchar(20) DEFAULT 'F' AFTER topt;
-ALTER TABLE atom_topic ADD rfrom varchar(255) AFTER rflag;
-
-ALTER TABLE atom_topic ADD mflag varchar(20) DEFAULT 'F' AFTER rfrom;
-ALTER TABLE atom_topic ADD mpath varchar(20) AFTER mflag;
-ALTER TABLE atom_topic CHANGE topt tflag varchar(20) DEFAULT 'F';
-
-ALTER TABLE atom_topic DROP COLUMN poster_id;
-ALTER TABLE atom_topic DROP COLUMN poster_name;
-
 /**
  * 评论数据表
  */
