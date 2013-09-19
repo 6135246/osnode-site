49787e8d06fee1b5036f29452adf854565b7841a
老牛啊 <obullxl@gmail.com>
2013-09-19 20:40:15 +0800

    set setup bat Signed-off-by: 老牛啊 <obullxl@gmail.com>

--- start.bat
+++ start.bat
@@ -1,3 +1,8 @@
-set PORT=80
+set APP_PORT=80
 set NODEHOST=http://obullxl.github.io
+set DB_NAME=r631obullxl
+set DB_HOST=r7821obullxl.mysql.aliyun.com
+set DB_PORT=3306
+set DB_USER=r631obullxl
+set DB_PASSWD=rd828439b
 node index.js
