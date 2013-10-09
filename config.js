6e9ace4e5104ae3bf071ab66b78d0e7264dff2e6
老牛啊 <obullxl@gmail.com>
2013-10-09 09:32:17 +0800

    add log framework

--- config.js
+++ config.js
@@ -2,6 +2,7 @@
  * 应用配置信息
  */
 module.exports = {
+	/* 静态资源 */
 	'static_host': process.env.STATIC_HOST || 'http://obullxl.github.io',
 	
 	/* 数据库配置参数 */
@@ -12,6 +13,10 @@ module.exports = {
 	'db_passwd': process.env.BAE_ENV_SK || process.env.DB_PASSWD,
 	'db_charset': 'UTF8',
 	'db_conn_limit': 5,
+	
+	/* 日志配置 */
+	'log_type': 'console',
+	'log_level': 1, // 0-TRACE, 1-DEBUG, 2-INFO, 3-WARN, 4-ERROR, 5-FETAL
 	
 	'ads_show': process.env.ADS_SHOW || true,
 	
